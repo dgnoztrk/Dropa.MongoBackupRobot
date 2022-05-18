e680c5516731c4e1f213199babf79bf5fee2b733
Doğan ÖZTÜRK <info@doganozturk.net>
2022-05-18 15:56:45 +0300

    Add project files.

---
+++ Dropa.MongoBackupRobot/Backup.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Dropa.MongoBackupRobot
+{
+    partial class Backup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Backup));
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstLog = new System.Windows.Forms.ListBox();
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.tmrBackup = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDbName = new System.Windows.Forms.TextBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(93, 43);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(256, 23);
+            this.txtIP.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP : ";
+            // 
+            // lstLog
+            // 
+            this.lstLog.FormattingEnabled = true;
+            this.lstLog.ItemHeight = 15;
+            this.lstLog.Location = new System.Drawing.Point(6, 302);
+            this.lstLog.Name = "lstLog";
+            this.lstLog.Size = new System.Drawing.Size(498, 334);
+            this.lstLog.TabIndex = 2;
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Location = new System.Drawing.Point(6, 642);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(532, 23);
+            this.btnStartStop.TabIndex = 3;
+            this.btnStartStop.Text = "Start";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 284);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "LOGS";
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(450, 273);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(54, 23);
+            this.btnTest.TabIndex = 5;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Output Path : ";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(93, 12);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(351, 23);
+            this.txtPath.TabIndex = 7;
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(450, 11);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(54, 23);
+            this.btnSelectFolder.TabIndex = 8;
+            this.btnSelectFolder.Text = "Select";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // tmrBackup
+            // 
+            this.tmrBackup.Interval = 3600000;
+            this.tmrBackup.Tick += new System.EventHandler(this.tmrBackup_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "DB Name : ";
+            // 
+            // txtDbName
+            // 
+            this.txtDbName.Location = new System.Drawing.Point(93, 78);
+            this.txtDbName.Name = "txtDbName";
+            this.txtDbName.Size = new System.Drawing.Size(256, 23);
+            this.txtDbName.TabIndex = 10;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(93, 116);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(256, 23);
+            this.txtUserName.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Username : ";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(93, 157);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(256, 23);
+            this.txtPassword.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 160);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 15);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Password : ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 203);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 15);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Port : ";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(93, 200);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(256, 23);
+            this.txtPort.TabIndex = 16;
+            // 
+            // Backup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 668);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDbName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnStartStop);
+            this.Controls.Add(this.lstLog);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIP);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Backup";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mongo DB Auto Backup";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtIP;
+        private Label label1;
+        private ListBox lstLog;
+        private Button btnStartStop;
+        private Label label2;
+        private Button btnTest;
+        private Label label3;
+        private TextBox txtPath;
+        private Button btnSelectFolder;
+        private System.Windows.Forms.Timer tmrBackup;
+        private Label label4;
+        private TextBox txtDbName;
+        private TextBox txtUserName;
+        private Label label5;
+        private TextBox txtPassword;
+        private Label label6;
+        private Label label7;
+        private TextBox txtPort;
+    }
+}
---
+++ Dropa.MongoBackupRobot/Backup.cs
@@ -0,0 +1,113 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Diagnostics;
+
+namespace Dropa.MongoBackupRobot
+{
+    public partial class Backup : Form
+    {
+        public Backup()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnStartStop_Click(object sender, EventArgs e)
+        {
+            Start();
+        }
+
+        private void SetLog(string log)
+        {
+            if (string.IsNullOrEmpty(log)) return;
+            lstLog.Items.Add(log);
+            lstLog.SelectedIndex = lstLog.Items.Count - 1;
+            lstLog.SelectedIndex = -1;
+        }
+
+        //private async Task Archive()
+        //{
+        //    string name = $"{DateTime.Now}_{Guid.NewGuid()}";
+        //    string fullfilename = Path.Combine(txtPath.Text, name);
+        //    ZipFile.CreateFromDirectory(txtPath.Text, fullfilename, CompressionLevel.Optimal, false);
+        //    SetLog($"Success -Archive : {name}");
+        //}
+
+        private async Task BackupUp()
+        {
+            string strCmdText = $@"mongodump --host  {txtIP.Text} -d {txtDbName.Text} --port {txtPort.Text} --username {txtUserName.Text} --password {txtPassword.Text} --archive={txtPath.Text}\\{DateTime.Now.ToString("yyyy'-'MM'-'dd'T'HH'-'mm'-'ss")}-{Guid.NewGuid()}.gz --gzip";
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+            cmd.StandardInput.WriteLine(strCmdText);
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+            Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+            SetLog($"Success -BackupUp {DateTime.Now}");
+        }
+
+        private async void tmrBackup_Tick(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtPath.Text))
+            {
+                Stop();
+            }
+            else
+            {
+                await BackupUp();
+            }
+        }
+
+        private void Stop()
+        {
+            tmrBackup.Stop();
+            tmrBackup.Enabled = false;
+        }
+
+        private void Start()
+        {
+            tmrBackup.Enabled = true;
+            tmrBackup.Start();
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    txtPath.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            string connectionStr = $"mongodb://{txtUserName.Text}:{txtPassword.Text}@{txtIP.Text}:{txtPort.Text}/{txtDbName.Text}";
+            var client = new MongoClient(connectionStr);
+            var server = client.GetDatabase(txtDbName.Text);
+            try
+            {
+                bool isMongoLive = server.RunCommandAsync((Command<BsonDocument>)"{ping:1}").Wait(1000);
+                if (isMongoLive)
+                {
+                    MessageBox.Show($"Success : Mongo is live");
+                }
+                else
+                {
+                    MessageBox.Show("Fail : Connection error");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Fail : {ex.Message}");
+            }
+        }
+    }
+}
